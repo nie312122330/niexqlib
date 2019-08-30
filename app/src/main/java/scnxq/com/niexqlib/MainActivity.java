5c62f08dce60d583bfe72aa38cb76c8302802d9d
nie312122330 <niexiaoqiang@sina.cn>
2019-08-30 11:01:20 +0800

    首次提交

---
+++ app/src/main/java/scnxq/com/niexqlib/MainActivity.java
@@ -0,0 +1,13 @@
+package scnxq.com.niexqlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ appbase/src/main/java/scnxq/com/appbase/AppBase.java
@@ -0,0 +1,8 @@
+package scnxq.com.appbase;
+
+public class AppBase {
+
+    protected String test1() {
+        return "ss";
+    }
+}
