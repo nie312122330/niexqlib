85c0ce5f141913aad6b8319b7ff5733136e0a6dc
niexiaoqiang <niexiaoqiang@sina.cn>
2019-12-29 22:31:14 +0800

    字符集

--- appbase/src/main/java/scnxq/com/appbase/utils/AssertFileUtils.java
+++ appbase/src/main/java/scnxq/com/appbase/utils/AssertFileUtils.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * AssertFile工具类
@@ -27,7 +28,7 @@ public class AssertFileUtils {
             byte[] bytes = consumeInputStream(open);
             String result = null;
             if (null != bytes) {
-                result = new String(bytes);
+                result = new String(bytes, StandardCharsets.UTF_8);
             }
             open.close();
             return result;
